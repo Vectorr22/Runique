8095da505065a5040c8e7dbc4d337a4bacc2a655
vectorr22 <vmtg208@hotmail.com>
2025-01-11 12:04:43 -0600

    minor changed to the RunOverViewState

--- run/presentation/src/main/java/com/vectorr22/run/presentation/run_overview/RunOverviewState.kt
+++ run/presentation/src/main/java/com/vectorr22/run/presentation/run_overview/RunOverviewState.kt
@@ -2,6 +2,6 @@ package com.vectorr22.run.presentation.run_overview
 
 import com.vectorr22.run.presentation.run_overview.module.RunUi
 
-data class RunOverviewState(
+data class RunOverViewState(
     val runs: List<RunUi> = emptyList()
 )
